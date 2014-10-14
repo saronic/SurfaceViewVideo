b3272af77b15c4a102bb1d8681ec2e0e1e4e744b
asus_win7 <asus_win7@saronic.com>
2014-10-14 14:36:39 +0800

    可以正常运行

--- src/fei/surfaceviewvideo/MainActivity.java
+++ src/fei/surfaceviewvideo/MainActivity.java
@@ -1,10 +1,13 @@
 package fei.surfaceviewvideo;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,6 +17,9 @@ public class MainActivity extends Activity {
 	private SurfaceView mSurfaceView;
 	private Button mBtnPlay;
 	private MediaPlayer mMediaPlayer;
+	private Button mBtnStop;
+	private Button mBtnPause;
+	private SurfaceHolder mSurfaceHolder;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +27,75 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
 		mBtnPlay = (Button) findViewById(R.id.btn_play);
+		mBtnStop = (Button) findViewById(R.id.btn_stop);
+		mBtnPause = (Button) findViewById(R.id.btn_pause);
 
-		final SurfaceHolder surfaceHolder = mSurfaceView.getHolder();
-		surfaceHolder.setFixedSize(100, 100);
-		surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-		mMediaPlayer = new MediaPlayer();
-		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		mSurfaceHolder = mSurfaceView.getHolder();
+		//mSurfaceHolder.setFixedSize(100, 100);
+		//mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 		
-		
+
 		mBtnPlay.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				
-				mMediaPlayer.setDisplay(surfaceHolder);
-				try {
-					mMediaPlayer.setDataSource("sdcard/video.3gp");
-					mMediaPlayer.prepare();
-					mMediaPlayer.start();
-				} catch (Exception e) {
-				}
+				start();
 			}
 		});
+		
+		mBtnPause.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (mMediaPlayer != null) {
+					if (mBtnPause.getText().equals("pause")) {
+						mMediaPlayer.pause();
+						mBtnPause.setText("continue");
+					} else {
+						mMediaPlayer.start();
+						mBtnPause.setText("pause");
+					}
+				}
+				
+			}
+		});
+		mBtnStop.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				stop();
+				
+			}
+		});
+	}
+
+	private void stop() {
+		
+		if (mMediaPlayer != null) {
+			mMediaPlayer.stop();
+			mMediaPlayer.release();
+			mMediaPlayer = null;
+		}
+	}
+
+	private void start() {
+		stop();
+		mMediaPlayer = new MediaPlayer();
+		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		mMediaPlayer.setDisplay(mSurfaceHolder);
+		try {
+			mMediaPlayer.setDataSource("sdcard/video.3gp");
+			mMediaPlayer.prepare();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		mMediaPlayer.start();
 	}
 
 }
